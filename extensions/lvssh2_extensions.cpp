533902fff34319d55257a705a16f4312c1970162
Pascal Neuperger <logmanoriginal@users.noreply.github.com>
2024-10-03 16:51:48 +0200

    extensions: Improve type-safety using libssh2 typedefs and macros
    libssh2 defines several macros and typedefs for callback functions
    that we can utilize to ensure type-safety. This applies to all of
    the callback functions except lvssh2_trace_handler_function, for
    which libssh2 only defines a typedef. Nonetheless, using the typedef
    ensures type parity when returning the function pointer.

--- extensions/lvssh2_extensions.cpp
+++ extensions/lvssh2_extensions.cpp
@@ -1,39 +1,17 @@
 #include "lvssh2_extensions.h"
 
-void lvssh2_trace_handler_function(LIBSSH2_SESSION* session, LVUserEventRef* event, const char* data, size_t length) {
+void lvssh2_trace_handler_function(LIBSSH2_SESSION* session, void* context, const char* data, size_t length) {
 	LStrHandle message = nullptr;
 	data_buffer_to_LStrHandle(data, length, &message);
 
-	PostLVUserEvent(*event, &message);
+	LVUserEventRef* e = (LVUserEventRef*)context;
+
+	PostLVUserEvent(*e, &message);
 
 	DSDisposeHandle(message);
 }
 
-int lvssh2_userauth_publickey_sign_function(LIBSSH2_SESSION* session, unsigned char** signature, size_t* signature_len, const unsigned char* data, size_t data_len, LVUserEventRef* event) {
-	*signature = nullptr;
-	*signature_len = 0;
-	
-	lvssh2_userauth_publickey_sign_function_input_args payload = { 0 };
-	payload.data = 0;
-
-	LStrHandle lv_signature = 0;
-	payload.signature = &lv_signature;
-
-	data_buffer_to_LStrHandle(data, data_len, &payload.data);
-
-	PostLVUserEvent(*event, &payload);
-
-	*signature = (unsigned char*)malloc(LHStrLen(lv_signature) * sizeof(unsigned char*));
-	memcpy(*signature, LHStrBuf(lv_signature), LHStrLen(lv_signature));
-
-	*signature_len = LHStrLen(lv_signature);
-
-	DSDisposeHandle(payload.data);
-
-	return 0;
-}
-
-ssize_t lvssh2_session_callback_send_function(libssh2_socket_t socket, const void* buffer, size_t length, int flags, lvssh2_abstract** abstract) {
+LIBSSH2_SEND_FUNC(lvssh2_session_callback_send_function) {
 	lvssh2_session_callback_send_function_input_args payload = { 0 };
 	payload.socket = socket;
 	payload.buffer = 0;
@@ -44,14 +22,16 @@ ssize_t lvssh2_session_callback_send_function(libssh2_socket_t socket, const voi
 
 	data_buffer_to_LStrHandle(buffer, length, &payload.buffer);
 
-	PostLVUserEvent((*abstract)->send, &payload);
+	lvssh2_abstract** lv_abstract = (lvssh2_abstract**)abstract;
+
+	PostLVUserEvent((*lv_abstract)->send, &payload);
 
 	DSDisposeHandle(payload.buffer);
 
 	return bytes_send;
 }
 
-ssize_t lvssh2_session_callback_recv_function(libssh2_socket_t socket, void* buffer, size_t length, int flags, lvssh2_abstract** abstract) {
+LIBSSH2_RECV_FUNC(lvssh2_session_callback_recv_function) {
 	lvssh2_session_callback_recv_function_input_args payload = { 0 };
 	payload.socket = socket;
 	payload.buffer = buffer;
@@ -61,21 +41,14 @@ ssize_t lvssh2_session_callback_recv_function(libssh2_socket_t socket, void* buf
 	ssize_t bytes_received = 0;
 	payload.bytes_received = &bytes_received;
 
-	PostLVUserEvent((*abstract)->recv, &payload);
+	lvssh2_abstract** lv_abstract = (lvssh2_abstract**)abstract;
+
+	PostLVUserEvent((*lv_abstract)->recv, &payload);
 
 	return bytes_received;
 }
 
-void lvssh2_userauth_keyboard_interactive_response_function(
-	const char* name,
-	int name_len,
-	const char* instruction,
-	int instruction_len,
-	int num_prompts,
-	const LIBSSH2_USERAUTH_KBDINT_PROMPT* prompts,
-	LIBSSH2_USERAUTH_KBDINT_RESPONSE* responses,
-	void** abstract)
-{
+LIBSSH2_USERAUTH_KBDINT_RESPONSE_FUNC(lvssh2_userauth_keyboard_interactive_response_function) {
 	if (num_prompts == 0) {
 		return;
 	}
@@ -117,6 +90,32 @@ void lvssh2_userauth_keyboard_interactive_response_function(
 
 	DSDisposeHandle(lv_name);
 	DSDisposeHandle(lv_instruction);
+}
+
+LIBSSH2_USERAUTH_PUBLICKEY_SIGN_FUNC(lvssh2_userauth_publickey_sign_function) {
+	*sig = nullptr;
+	*sig_len = 0;
+
+	lvssh2_userauth_publickey_sign_function_input_args payload = { 0 };
+	payload.data = 0;
+
+	LStrHandle lv_signature = 0;
+	payload.signature = &lv_signature;
+
+	data_buffer_to_LStrHandle(data, data_len, &payload.data);
+
+	LVUserEventRef* e = (LVUserEventRef*)abstract;
+
+	PostLVUserEvent(*e, &payload);
+
+	*sig = (unsigned char*)malloc(LHStrLen(lv_signature) * sizeof(unsigned char*));
+	memcpy(*sig, LHStrBuf(lv_signature), LHStrLen(lv_signature));
+
+	*sig_len = LHStrLen(lv_signature);
+
+	DSDisposeHandle(payload.data);
+
+	return 0;
 }
 
 void data_buffer_to_LStrHandle(const void* data, size_t data_length, LStrHandle* string_handle_ptr) {
--- extensions/lvssh2_extensions.h
+++ extensions/lvssh2_extensions.h
@@ -40,31 +40,35 @@ typedef struct {
 
 LVUserEventRef* lvssh2_userauth_keyboard_interactive_response_event = { 0 };
 
+// Function pointer for the userauth publickey sign function
+typedef LIBSSH2_USERAUTH_PUBLICKEY_SIGN_FUNC((*LIBSSH2_USERAUTH_PUBLICKEY_SIGN_FUNC_PTR));
+
+// Function pointer for the userauth keyboard interactive response function
+typedef LIBSSH2_USERAUTH_KBDINT_RESPONSE_FUNC((*LIBSSH2_USERAUTH_KBDINT_RESPONSE_FUNC_PTR));
+
+// Function pointer for the session callback send function
+typedef LIBSSH2_SEND_FUNC((*LIBSSH2_SEND_FUNC_PTR));
+
+// Function pointer for the session callback recv function
+typedef LIBSSH2_RECV_FUNC((*LIBSSH2_RECV_FUNC_PTR));
+
 #include "lv_epilog.h"
 
 void data_buffer_to_LStrHandle(const void*, size_t, LStrHandle*);
 
-void lvssh2_trace_handler_function(LIBSSH2_SESSION* session, LVUserEventRef* event, const char* data, size_t length);
-int lvssh2_userauth_publickey_sign_function(LIBSSH2_SESSION* session, unsigned char** signature, size_t* signature_len, const unsigned char* data, size_t data_len, LVUserEventRef* event);
+void lvssh2_trace_handler_function(LIBSSH2_SESSION* session, void* context, const char* data, size_t length);
 
-ssize_t lvssh2_session_callback_send_function(libssh2_socket_t socket, const void* buffer, size_t length, int flags, lvssh2_abstract** abstract);
-ssize_t lvssh2_session_callback_recv_function(libssh2_socket_t socket, void* buffer, size_t length, int flags, lvssh2_abstract** abstract);
+LIBSSH2_SEND_FUNC(lvssh2_session_callback_send_function);
+LIBSSH2_RECV_FUNC(lvssh2_session_callback_recv_function);
 
-void lvssh2_userauth_keyboard_interactive_response_function(
-	const char* name,
-	int name_len,
-	const char* instruction,
-	int instruction_len,
-	int num_prompts,
-	const LIBSSH2_USERAUTH_KBDINT_PROMPT* prompts,
-	LIBSSH2_USERAUTH_KBDINT_RESPONSE* responses,
-	void** abstract);
+LIBSSH2_USERAUTH_KBDINT_RESPONSE_FUNC(lvssh2_userauth_keyboard_interactive_response_function);
+LIBSSH2_USERAUTH_PUBLICKEY_SIGN_FUNC(lvssh2_userauth_publickey_sign_function);
 
-extern "C" __declspec(dllexport) void* get_lvssh2_trace_handler_function() { return lvssh2_trace_handler_function; }
-extern "C" __declspec(dllexport) void* get_lvssh2_userauth_publickey_sign_function() { return lvssh2_userauth_publickey_sign_function; }
+extern "C" __declspec(dllexport) libssh2_trace_handler_func get_lvssh2_trace_handler_function() { return lvssh2_trace_handler_function; }
+extern "C" __declspec(dllexport) LIBSSH2_USERAUTH_PUBLICKEY_SIGN_FUNC_PTR get_lvssh2_userauth_publickey_sign_function() { return lvssh2_userauth_publickey_sign_function; }
 
-extern "C" __declspec(dllexport) void* get_lvssh2_session_callback_send_function() { return lvssh2_session_callback_send_function; }
-extern "C" __declspec(dllexport) void* get_lvssh2_session_callback_recv_function() { return lvssh2_session_callback_recv_function; }
+extern "C" __declspec(dllexport) LIBSSH2_SEND_FUNC_PTR get_lvssh2_session_callback_send_function() { return lvssh2_session_callback_send_function; }
+extern "C" __declspec(dllexport) LIBSSH2_RECV_FUNC_PTR get_lvssh2_session_callback_recv_function() { return lvssh2_session_callback_recv_function; }
 
-extern "C" __declspec(dllexport) void* get_lvssh2_userauth_keyboard_interactive_response_function() { return lvssh2_userauth_keyboard_interactive_response_function; }
+extern "C" __declspec(dllexport) LIBSSH2_USERAUTH_KBDINT_RESPONSE_FUNC_PTR get_lvssh2_userauth_keyboard_interactive_response_function() { return lvssh2_userauth_keyboard_interactive_response_function; }
 extern "C" __declspec(dllexport) void set_lvssh2_userauth_keyboard_interactive_response_callback(LVUserEventRef* event) { lvssh2_userauth_keyboard_interactive_response_event = event; }
