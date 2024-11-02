75bd7e104f4bbdebe945a03050d57c2d7a56a926
Pascal Neuperger <logmanoriginal@users.noreply.github.com>
2024-11-02 19:47:59 +0100

    build.sh: replace release.sh, add platform support for Windows and Linux
    This replaces release.sh and adds platform support to build lvssh2 on
    Windows and Linux. It supports a configuration option to specify whether
    to make a Debug or Release build. For Debug builds, the GitHub
    repository is used (this is to avoid the signature check). The Release
    build works as previously.

--- release.sh
+++
@@ -1,7 +0,0 @@
-cmake -B build/x86 -S . -G "Visual Studio 17 2022" -A Win32 -DLIBSSH2_SOURCE=Tarball -DLIBSSH2_URL=https://libssh2.org/download/libssh2-1.11.1.tar.gz -DLIBSSH2_URL_SIG=https://libssh2.org/download/libssh2-1.11.1.tar.gz.asc
-
-cmake -B build/x64 -S . -G "Visual Studio 17 2022" -A x64 -DLIBSSH2_SOURCE=Tarball -DLIBSSH2_URL=https://libssh2.org/download/libssh2-1.11.1.tar.gz -DLIBSSH2_URL_SIG=https://libssh2.org/download/libssh2-1.11.1.tar.gz.asc
-
-cmake --build build/x86 --config Release
-
-cmake --build build/x64 --config Release
