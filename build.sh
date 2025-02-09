ae8bd3462c5b4c7be6f7b3c8af3da145ff83d392
Pascal Neuperger <logmanoriginal@users.noreply.github.com>
2025-02-09 11:04:41 +0100

    build.sh: Add crypto_backend option
    This adds an option to specify the crypto backend. On Windows, the
    WinCNG backend is much faster as it does not need to be build from
    source. The default backend is OpenSSL.

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 # This script builds the project for different OS types.
 # It supports Windows (msys) and Linux (linux-gnu).
 # Options:
-#   - build.sh [Release|Debug]
+#   - build.sh [Release|Debug] [OpenSSL|WinCNG]
 
 build_type=${1:-Release}
+crypto_backend=${2:-OpenSSL}
 
 if [[ "$OSTYPE" == "msys"* ]]; then
     generator="Visual Studio 17 2022"
@@ -19,9 +20,11 @@ fi
 for arch in "${build_archs[@]}"; do
     echo "Building $arch..."
     cmake_cmd="cmake -B build/$arch -S . -G \"$generator\""
+
     if [[ "$generator" == "Visual Studio 17 2022" ]]; then
         cmake_cmd+=" -A \"$arch\""
     fi
+
     if [[ "$build_type" == "Release" ]]; then
         cmake_cmd+=" -DLIBSSH2_SOURCE=Tarball"
         cmake_cmd+=" -DLIBSSH2_URL=https://libssh2.org/download/libssh2-1.11.1.tar.gz"
@@ -30,6 +33,12 @@ for arch in "${build_archs[@]}"; do
         cmake_cmd+=" -DCMAKE_BUILD_TYPE=Debug"
         cmake_cmd+=" -DLIBSSH2_SOURCE=GitHub"
         cmake_cmd+=" -DLIBSSH2_COMMIT_HASH=libssh2-1.11.1"
+    fi
+
+    cmake_cmd+=" -DCRYPTO_BACKEND=$crypto_backend"
+
+    if [[ "$crypto_backend" == "OpenSSL" ]]; then
+        cmake_cmd+=" -DOPENSSL_COMMIT_HASH=openssl-3.4.0"
     fi
 
     eval $cmake_cmd
